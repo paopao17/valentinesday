5ef9e3b0016a3610921df90b597cbc759d586734
kelivreno <hakim.kelangit@student.greenriver.edu>
2023-02-06 14:40:36 -0800

    for poi

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yes").addEventListener("click", function() {
+    window.location.href = "event.html";
+  });
+  
+  document.getElementById("no").addEventListener("click", function() {
+    window.location.href = "sad.html";
+  });
